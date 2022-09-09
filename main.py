3f230cba1dbc85cb16f68dfe59b43722d95157c1
dengle1234 <53254035+dengle1234@users.noreply.github.com>
2022-09-09 09:53:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import pytest
-from case import *
-
-
-if __name__ == '__main__':
-    pytest.main(['-s', 'D:/demo/HTT-Selenium/case/Test_Warehousing_prediction.py --html=report.html'])
-
+print('test111')
